f187fdd0b4056cce9dddf76ff3c65ff02626d0d5
Cesar Adrian Leyva Luna <72633350+BookmanTasty@users.noreply.github.com>
2022-04-12 03:10:29 -0500

    add docs

---
+++ docs/build/html/_sources/api/dummysqlauthorizer.rst.txt
@@ -0,0 +1,9 @@
+==============
+DummySqlAuthorizer
+==============
+
+.. currentmodule:: pyftpdlibsqladdon.dummysqlauthorizer
+
+.. Don't include inherited members to keep the doc short
+.. autoclass:: pyftpdlibsqladdon.DummySqlAuthorizer
+    :members:
---
+++ docs/source/conf.py
@@ -0,0 +1,26 @@
+
+# -- Path setup ----------------------------------------
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'pyftpdlibsqladdon'
+copyright = '2022, Cesar Adrian Leyva Luna'
+author = 'Cesar Adrian Leyva Luna'
+
+
+# -- General configuration -----------------------------
+extensions = [
+    'recommonmark',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output ---------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
